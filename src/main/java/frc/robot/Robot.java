4333c5e8c2240bf4bff15be38ec156145a767bfc
jvelez3970@gmail.com <jvelez3970@gmail.com>
2020-10-13 19:43:50 -0700

    PrettyMuchDone

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 //You probably already knew that but pointless comments never hurt anyone
 import edu.wpi.first.wpilibj.Joystick;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 /**
@@ -36,8 +37,16 @@ public class Robot extends TimedRobot {
   //Normally this would be things like motor controllers, sensors, and joysticks
   @Override
   public void robotInit() {
+    //Creates a joystick at ID 0, joystick IDS are set within driver station
     gamePad = new Joystick(0);
 
+    //Creates a TalonSRX With a device ID of 0
+    //Device IDs can be set within the phoenix tuner, and Each talon must have a unique device ID
+    //To determine which ID a given talon should be set to you can use the flash command within the tuner to identify where a given talon is
+    driveTrainLeft1 = new TalonSRX(0);
+    driveTrainLeft2 = new TalonSRX(1);
+    driveTrainRight1 = new TalonSRX(2);
+    driveTrainRight2 = new TalonSRX(3);
 
   }
 
@@ -51,10 +60,17 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopInit() {
+
   }
 
   @Override
   public void teleopPeriodic() {
+    //Axis 0 on a gamepad is the leftstcks Y axis output, 5 is the rightsticks Y axis output
+    driveTrainLeft1.set(ControlMode.PercentOutput, gamePad.getRawAxis(0));
+    driveTrainLeft2.set(ControlMode.PercentOutput, gamePad.getRawAxis(0));
+    //The negative inthe input is to account for the flip in directionality that one side of the drivetrain is to the other
+    driveTrainRight1.set(ControlMode.PercentOutput, -1*(gamePad.getRawAxis(5)));
+    driveTrainRight2.set(ControlMode.PercentOutput, -1*(gamePad.getRawAxis(5)));
   }
 
   @Override
